a2077c639f214820961a6a9820405f0bc4893f13
ivan.laukhin <a700ac@gmail.com>
2023-11-13 19:23:34 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -29,34 +29,33 @@ public class Main {
         //Разделение строки на числа и действие;
         String[] arr=a.split("[+\\-*/]");
         int b=arr.length;
+        //Проверки на правильность ввода 1;
         if (b>2) throw new Exception("Формат математической операции не удовлетворяет заданию - два операнда " +
                 "и один оператор (+, -, /, *)");
         if (b<2) throw new Exception("Cтрока не является математической операцией");
 
-        //Поиск римских операндов;
+        //Поиск римских операндов и присваивание им арабских значений;
         for (int i=0;i<rim.length; i++) {
             if  (arr[0].equals(rim[i])) { operand1 = i; chk1=true; }
         }
         for (int i=0;i<rim.length; i++) {
             if (arr[1].equals(rim[i])) { operand2 = i; chk2=true;}
         }
-        //Проверки на правильность ввода;
+        //Проверки на правильность ввода 2;
         if ((chk1==true)&&(chk2==false)||((chk1==false)&&(chk2==true))) {throw new Exception("Используются одновременно" +
                 " разные системы счисления"); }
 
-
+        //Присваивания значений арабским операндам;
         if (chk1==false) {operand1 = Integer.parseInt(arr[0]);}
         if (chk2==false) {operand2 = Integer.parseInt(arr[1]);}
-
+        //Проверки на правильность ввода 3;
         if ((operand1>10)||(operand2>10)||(operand1==0)||(operand2==0)) throw new Exception("Вводимые числа могут быть" +
                 " от 1 до 10");
-
+        //Выполнение действия и вывод на консоль;
         if (a.contains("+")) {res=operand1+operand2;}
         if (a.contains("-")) {res=operand1-operand2;}
         if (a.contains("*")) {res=operand1*operand2;}
         if (a.contains("/")) {res = operand1 / operand2;}
-
-        System.out.println(res);
 
         if ((chk1==true)&&(chk2==true)&&(res<1))  throw new Exception("В римской системе нет отрицательных чисел и нуля");
             if ((chk1==true)&&(chk2==true))  {System.out.println(rim[res]);
